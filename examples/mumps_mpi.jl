21f30f79a5d34ede6316bbde8847fced4de08b3d
Dominique Orban <dominique.orban@gmail.com>
2014-08-17 16:16:49 -0400

    Examples using MPI.

---
+++ examples/mumps_mpi.jl
@@ -0,0 +1,24 @@
+using MUMPS
+import MPI
+
+function main(A, rhs)
+
+  MPI.init()
+
+  mumps = Mumps(2);  # General symmetric.
+  factorize(mumps, A);
+  x = solve(mumps, rhs);
+  finalize(mumps);
+
+  MPI.barrier(MPI.COMM_WORLD)
+  MPI.finalize()
+  return x
+end
+
+A = rand(4,4); A = sparse(A + A');
+rhs = rand(4);
+x = main(A, rhs);
+rel_err = norm(x - A\rhs) / norm(x);
+
+# The following usually comes out all garbled.
+@printf("Error: %7.1e\n", rel_err);
