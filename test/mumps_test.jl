fbced612a27bb075987e685eb04210924e0b698a
Dominique Orban <dominique.orban@gmail.com>
2014-12-31 22:07:58 -0500

    Port test to updated API.

--- test/mumps_test.jl
+++ test/mumps_test.jl
@@ -1,10 +1,13 @@
 using Base.Test
 using MUMPS
+using MPI
 
 include("get_div_grad.jl");
+root = 0;
 
-# Initialize MPI. Could also use MPI.jl.
-mumps_initialize_mpi();
+# Initialize MPI.
+MPI.Init()
+comm = MPI.COMM_WORLD
 
 icntl = default_icntl[:];
 #=icntl[1] = 0;=#
@@ -12,28 +15,31 @@ icntl = default_icntl[:];
 #=icntl[3] = 0;=#
 #=icntl[4] = 0;=#
 
-mumps1 = Mumps(1, icntl=icntl);    # Initialize for symmetric definite.
+mumps1 = Mumps(mumps_definite, icntl=icntl);
 A = spdiagm([1., 2., 3., 4.]);
 factorize(mumps1, A);  # Analyze and factorize.
 rhs = [1., 4., 9., 16.];
 x = solve(mumps1, rhs);
 finalize(mumps1);
+MPI.Barrier(comm)
 @test(norm(x - [1, 2, 3, 4]) <= 1.0e-14);
 
-mumps2 = Mumps(2, icntl=icntl);  # General symmetric.
+mumps2 = Mumps(mumps_symmetric, icntl=icntl);
 A = rand(4,4); A = sparse(A + A');
 factorize(mumps2, A);
 rhs = rand(4);
 x = solve(mumps2, rhs);
 finalize(mumps2);
+MPI.Barrier(comm)
 @test(norm(x - A\rhs)/norm(x) <= 1.0e-12);
 
-mumps3 = Mumps(0, icntl=icntl);  # General unsymmetric.
+mumps3 = Mumps(mumps_unsymmetric, icntl=icntl);
 A = sparse(rand(4,4));
 factorize(mumps3, A);
 rhs = rand(4);
 x = solve(mumps3, rhs);
 finalize(mumps3);
+MPI.Barrier(comm)
 @test(norm(x - A\rhs)/norm(x) <= 1.0e-12);
 
 # Test for solving real div-grad system with single and multiple rhs.
@@ -43,25 +49,32 @@ A = get_div_grad(32, 32, 32);
 n = size(A, 1);
 
 # Test with single rhs
-println("Test single rhs on div_grad matrix");
+if MPI.Comm_rank(comm) == root
+  println("Test single rhs on div_grad matrix");
+end
 rhs = randn(n);
 
-x = solve(A, rhs, 1);
+x = solve(A, rhs, sym=mumps_definite);
+MPI.Barrier(comm)
 relres = norm(A * x - rhs) / norm(rhs);
 @test(relres <= 1.0e-12);
 
 # Test with multiple rhs
-println("Test multiple rhs on div_grad matrix");
+if MPI.Comm_rank(comm) == root
+  println("Test multiple rhs on div_grad matrix");
+end
 nrhs = 10;
 rhs = randn(n, nrhs);
 
-x = solve(A, rhs, 1);
+x = solve(A, rhs, sym=mumps_definite);
 
+MPI.Barrier(comm)
 relres = zeros(nrhs)
 for i = 1 : nrhs
   relres[i] =  norm(A * x[:,i] - rhs[:,i]) / norm(rhs[:,i]);
   @test(relres[i] <= 1.0e-12);
 end
 
-mumps_finalize_mpi();
+MPI.Barrier(comm)
+MPI.Finalize()
 
