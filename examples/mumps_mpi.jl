a3d25b8bbd6c3cb67b8a86943fc174a8244debb6
Dominique Orban <dominique.orban@gmail.com>
2014-12-31 21:58:34 -0500

    Update example.

--- examples/mumps_mpi.jl
+++ examples/mumps_mpi.jl
@@ -1,24 +1,33 @@
 using MUMPS
 import MPI
 
-function main(A, rhs)
+MPI.Init()
+root = 0;
+comm = MPI.COMM_WORLD;
 
-  MPI.Init()
+mumps = Mumps(2);  # General symmetric.
 
-  mumps = Mumps(2);  # General symmetric.
-  factorize(mumps, A);
-  x = solve(mumps, rhs);
-  finalize(mumps);
-
-  MPI.Barrier(MPI.COMM_WORLD)
-  MPI.Finalize()
-  return x
+# Define problem on the host.
+if MPI.Comm_rank(comm) == root
+  A = rand(4,4); A = sparse(A + A');
+  associate_matrix(mumps, A);
+  rhs = rand(4);
+  associate_rhs(mumps, rhs);
 end
 
-A = rand(4,4); A = sparse(A + A');
-rhs = rand(4);
-x = main(A, rhs);
-rel_err = norm(x - A\rhs) / norm(x);
+# Solve problem in parallel.
+factorize(mumps);
+solve(mumps);
 
-# The following usually comes out all garbled.
-@printf("Error: %7.1e\n", rel_err);
+MPI.Barrier(comm)
+
+# By default, the solution is assembled and
+# overwrites rhs, so only exists on the host.
+if MPI.Comm_rank(comm) == root
+  x = get_solution(mumps);
+  rel_err = norm(x - A\rhs) / norm(x);
+  @printf("Error: %7.1e\n", rel_err);
+end
+
+finalize(mumps);
+MPI.Finalize()
