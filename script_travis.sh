49148f66d90aa6791a8686efe3650f0d1d0c5471
Dominique Orban <dominique.orban@gmail.com>
2015-01-18 23:35:45 -0500

    Remove debug commands.

--- script_travis.sh
+++ script_travis.sh
@@ -3,8 +3,5 @@ set -ev
 
 export JULIA_PKG_DIR=$(julia -E 'Pkg.dir()' | sed -e 's/"//g')
 export LD_LIBRARY_PATH=$JULIA_PKG_DIR/MUMPS/src:$LD_LIBRARY_PATH
-echo $LD_LIBRARY_PATH
-ls -l $JULIA_PKG_DIR/
-ls -l $JULIA_PKG_DIR/MUMPS/src
 julia --check-bounds=yes -E 'Pkg.test("MUMPS")'
 mpirun -np 2 julia $TRAVIS_BUILD_DIR/examples/mumps_mpi.jl
