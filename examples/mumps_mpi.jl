7109f251e8804fc50829303a7b4be5f0c3a8ee2f
Dominique Orban <dominique.orban@gmail.com>
2015-02-13 01:45:49 -0500

    Add complex MPI example.

--- examples/mumps_mpi.jl
+++ examples/mumps_mpi.jl
@@ -5,13 +5,41 @@ MPI.Init()
 root = 0;
 comm = MPI.COMM_WORLD;
 
-mumps = Mumps(2);  # General symmetric.
+# Solve a real problem.
+mumps = Mumps{Float64}(mumps_symmetric, default_icntl, default_cntl);
 
 # Define problem on the host.
 if MPI.Comm_rank(comm) == root
   A = rand(4,4); A = sparse(A + A');
   associate_matrix(mumps, A);
   rhs = rand(4);
+  associate_rhs(mumps, rhs);
+end
+
+# 1. Solve problem in parallel.
+factorize(mumps);
+solve(mumps);
+
+MPI.Barrier(comm)
+
+# By default, the solution is assembled and
+# overwrites rhs, so only exists on the host.
+if MPI.Comm_rank(comm) == root
+  x = get_solution(mumps);
+  rel_err = norm(x - A\rhs) / norm(x);
+  @printf("Error: %7.1e\n", rel_err);
+end
+
+finalize(mumps);
+
+# 2. Solve a complex problem.
+mumps = Mumps{Complex128}(mumps_unsymmetric, default_icntl, default_cntl);
+
+# Define problem on the host.
+if MPI.Comm_rank(comm) == root
+  A = rand(4,4) + im * rand(4,4); A = sparse(A + A');
+  associate_matrix(mumps, A);
+  rhs = rand(4) + im * rand(4);
   associate_rhs(mumps, rhs);
 end
 
