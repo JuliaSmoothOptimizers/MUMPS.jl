e6b3e99ccf6be338ef3b9bc81cd99c356c0912ec
Dominique Orban <dominique.orban@gmail.com>
2015-02-14 15:55:40 -0500

    More mixed tests.

--- test/mumps_test_mixed.jl
+++ test/mumps_test_mixed.jl
@@ -8,6 +8,22 @@ MPI.Barrier(comm);
 relres = norm(A * x - rhs) / norm(rhs);
 @test(relres <= 1.0e-12);
 
+# Here, MUMPS will run in complex single precision.
+mumps = Mumps{Complex64}(mumps_unsymmetric, default_icntl, default_cntl32);
+x = solve(mumps, A, rhs);
+MPI.Barrier(comm);
+finalize(mumps);
+relres = norm(A * x - rhs) / norm(rhs);
+@test(relres <= 1.0e-3);
+
+# Same, with sparse A.
+mumps = Mumps{Complex64}(mumps_unsymmetric, default_icntl, default_cntl32);
+x = solve(mumps, sparse(A), rhs);
+MPI.Barrier(comm);
+finalize(mumps);
+relres = norm(A * x - rhs) / norm(rhs);
+@test(relres <= 1.0e-3);
+
 # For now both A and rhs will be converted to Complex128.
 A = rand(Complex32, 4, 4);
 rhs = rand(Int16, 4);
