ea93eeb83fcde4e2797190437a1ea0268b69d33d
Dominique Orban <dominique.orban@gmail.com>
2015-02-14 19:33:19 -0500

    Test associate_matrix/rhs with mixed types.

--- test/mumps_test_mixed.jl
+++ test/mumps_test_mixed.jl
@@ -24,6 +24,30 @@ finalize(mumps);
 relres = norm(A * x - rhs) / norm(rhs);
 @test(relres <= 1.0e-3);
 
+# Same as above but call associate_matrix and associate_rhs directly.
+mumps = Mumps{Complex64}(mumps_unsymmetric, default_icntl, default_cntl32);
+associate_matrix(mumps, A);
+factorize(mumps);
+associate_rhs(mumps, rhs);
+solve(mumps);
+x = get_solution(mumps);
+MPI.Barrier(comm);
+finalize(mumps);
+relres = norm(A * x - rhs) / norm(rhs);
+@test(relres <= 1.0e-3);
+
+# And the same with sparse A.
+mumps = Mumps{Complex64}(mumps_unsymmetric, default_icntl, default_cntl32);
+associate_matrix(mumps, sparse(A));
+factorize(mumps);
+associate_rhs(mumps, rhs);
+solve(mumps);
+x = get_solution(mumps);
+MPI.Barrier(comm);
+finalize(mumps);
+relres = norm(A * x - rhs) / norm(rhs);
+@test(relres <= 1.0e-3);
+
 # For now both A and rhs will be converted to Complex128.
 A = rand(Complex32, 4, 4);
 rhs = rand(Int16, 4);
