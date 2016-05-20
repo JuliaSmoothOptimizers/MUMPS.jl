9069fe05ad3db315bbd6a1af355720c35f85495a
Dominique Orban <dominique.orban@gmail.com>
2016-05-20 11:57:41 -0400

    associate_rhs is not deprecated

--- src/MUMPS.jl
+++ src/MUMPS.jl
@@ -222,11 +222,6 @@ import Base.factorize
 @Base.deprecate factorize{Tm <: MUMPSValueDataType, Tv <: Number, Ti <: Integer}(mumps :: Mumps{Tm}, A :: SparseMatrixCSC{Tv,Ti}) factorize!(mumps, A)
 
 
-@Base.deprecate associate_rhs{Tv <: MUMPSValueDataType}(mumps :: Mumps{Tv}, rhs :: Array{Tv}) associate_rhs!(mumps, rhs)
-
-@Base.deprecate associate_rhs{Tm <: MUMPSValueDataType, Tv <: Number}(mumps :: Mumps{Tm}, rhs :: Array{Tv}) associate_rhs!(mumps, rhs);
-
-
 @Base.deprecate solve{Tv <: MUMPSValueDataType}(mumps :: Mumps{Tv}; transposed :: Bool=false) solve!(mumps, transposed=transposed)
 
 @Base.deprecate factorize{Tm <: MUMPSValueDataType, Tv <: Number}(mumps :: Mumps{Tm}, A :: Array{Tv}) factorize!(mumps, A)
