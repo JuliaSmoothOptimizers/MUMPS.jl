a2ae0b8b145ea824b84ee7670866f61ab78800a7
Dominique Orban <dominique.orban@gmail.com>
2017-08-03 23:58:37 -0400

    build with linuxbrew on linux

--- deps/build.jl
+++ deps/build.jl
@@ -6,34 +6,13 @@ using Compat
 libmumps = library_dependency("libmumps_common")
 libmumps_simple = library_dependency("libmumps_simple", depends=[libmumps])
 
-# Uncomment when MUMPS makes it into Homebrew.jl.
-@static if is_apple()
+if is_apple()
   using Homebrew
   provides(Homebrew.HB, "homebrew/science/mumps", [libmumps, libmumps_simple], os = :Darwin)
 end
-
-provides(AptGet, "libmumps-dev", libmumps, os = :Linux)
-
-provides(Sources,
-         URI("https://github.com/dpo/mumps_simple/archive/v0.4.tar.gz"),
-         libmumps_simple,
-         SHA="87d1fc87eb04cfa1cba0ca0a18f051b348a93b0b2c2e97279b23994664ee437e",
-         unpacked_dir="mumps_simple-0.4", os = :Unix)
-
-depsdir = BinDeps.depsdir(libmumps_simple)
-prefix = joinpath(depsdir, "usr")
-srcdir = joinpath(depsdir, "src", "mumps_simple-0.4")
-
-provides(SimpleBuild,
-         (@build_steps begin
-            GetSources(libmumps_simple)
-            (@build_steps begin
-               ChangeDirectory(srcdir)
-               (@build_steps begin
-                  `make mumps_prefix=/usr scalapack_libdir=/usr/lib scalapack_libs= blas_libs=`
-                  `make install prefix=$prefix`
-                end)
-             end)
-          end), [libmumps_simple], os = :Unix)
+if is_linux()
+  using Linuxbrew
+  provides(Linuxbrew.LB, "homebrew/science/mumps", [libmumps, libmumps_simple], os = :Linux)
+end
 
 @BinDeps.install Dict(:libmumps_simple => :libmumps_simple)
