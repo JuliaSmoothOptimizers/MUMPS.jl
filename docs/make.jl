6fe107d9baee982bdd74591162f3a99c7feb7057
Dominique Orban <dominique.orban@gmail.com>
2019-02-19 10:11:35 -0500

    add basic documentation

---
+++ docs/make.jl
@@ -0,0 +1,21 @@
+using Documenter, MUMPS
+
+makedocs(
+  modules = [MUMPS],
+  doctest = true,
+  linkcheck = true,
+  strict = true,
+  assets = ["assets/style.css"],
+  format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+  sitename = "MUMPS.jl",
+  pages = ["Home" => "index.md",
+           "API" => "api.md",
+           "Reference" => "reference.md",
+          ]
+)
+
+deploydocs(deps = nothing, make = nothing,
+  repo = "github.com/JuliaSmoothOptimizers/MUMPS.jl.git",
+  target = "build",
+  devbranch = "master"
+)
