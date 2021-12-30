bc7123957b73382ec9d273ccfe429a2bacb787f7
Dominique Orban <dominique.orban@gmail.com>
2021-12-30 12:54:24 -0500

    move module docstring to main module file

--- src/MUMPS_C-API.jl
+++ src/MUMPS_C-API.jl
@@ -1,40 +1,7 @@
-"""
-    module MUMPS
+using Libdl, LinearAlgebra, SparseArrays
 
-Both low-level interface with MUMPS 5.2.0 parallel direct solver C-library
-as well as convenient wrappers for some common uses for MUMPS.
+using MPI
 
-The central work is done by the `Mumps` struct, which mirrors the
-internal structure used in MUMPS. Manipulations can be done directly
-on this object and then passed to Mumps via the function [`invoke_mumps!`](@ref)
-This mode of operation gives the user complete control as described
-in the MUMPS manual, though it exposes unsafe operations, so beware.
-
-More convenient are the use of the functions [`mumps_solve`](@ref), [`mumps_factorize`](@ref),
-[`mumps_det`](@ref), [`mumps_schur`](@ref), and [`mumps_select_inv`](@ref), which all have
-mutating counterparts (such as [`mumps_solve!`](@ref)). These can take matrices
-and right hand sides directly, so, for example, the equation `A*x=y`, solved
-in Base by `x=A\\y` or `LinearAlbegra.ldiv!(x,A,y)`, can be solved in MUMPS3
-as `x=mumps_solve(A,y)`, or `mumps_solve!(x,A,y)`.
-
-The package also extends Base.det, Base.\\, LinearAlgebra.ldiv! and LinearAlgebra.inv to
-work with mumps objects.
-
-Note, unless working with the low-level interace, I discourage setting the `JOB`
-parameter manually, as this can lead to unsafe operation.
-
-The goal is to give the advanced user low-level access to MUMPS, while simultaneously
-giving the ordinary user safe functions that grant access to most of what
-MUMPS has to offer.
-
-(https://github.com/wrs28/MUMPS3.jl).
-"""
-# module MUMPS
-
-using MPI,
-Libdl,
-LinearAlgebra,
-SparseArrays
 
 function __init__()
   if isfile(joinpath(dirname(@__FILE__),"..","deps","deps.jl"))
@@ -56,5 +23,3 @@ include("icntl_alibis.jl")
 include("printing.jl")
 
 include("exported_methods.jl")
-
-# end
