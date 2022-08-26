76b1de17763f68abe2bdbba3fc5a3aee5c5e0d1a
Alexis Montoison <alexis.montoison@polymtl.ca>
2022-08-26 08:48:34 -0400

    Test MKL.jl

--- src/MUMPS.jl
+++ src/MUMPS.jl
@@ -45,13 +45,13 @@ else
   const MUMPS_INSTALLATION = "YGGDRASIL"
 end
 
-using OpenBLAS32_jll
+using MKL_jll
 
 function __init__()
   if VERSION ≥ v"1.7"
     config = LinearAlgebra.BLAS.lbt_get_config()
     if !any(lib -> lib.interface == :lp64, config.loaded_libs)
-      LinearAlgebra.BLAS.lbt_forward(OpenBLAS32_jll.libopenblas_path)
+      LinearAlgebra.BLAS.lbt_forward(MKL_jll.libmkl_rt_path)
     end
   end
 end
