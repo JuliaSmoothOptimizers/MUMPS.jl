8eaebac2c0ba040c84ee545dd2a16632f8c61ea9
amontoison <amontoison@users.noreply.github.com>
2025-10-22 21:41:53 +0000

    :robot: Format .jl files

--- test/mumps_test_scalapack.jl
+++ test/mumps_test_scalapack.jl
@@ -5,96 +5,96 @@ using MUMPS
 using SparseArrays
 
 @testset "ScaLAPACK enabled (ICNTL[13]=0)" begin
-    n = 20
-    A = sprand(Float64, n, n, 0.3) + I
-    A = A + A'
-    rhs = rand(Float64, n)
-    
-    icntl = default_icntl[:]
-    icntl[1:4] .= 0
-    icntl[13] = 0
-    
-    mumps = Mumps{Float64}(mumps_symmetric, icntl, default_cntl64)
-    
-    @test mumps.icntl[13] == 0
-    
-    factorize!(mumps, A)
-    x = solve(mumps, rhs)
-    
-    tol = sqrt(eps(Float64))
-    @test norm(A * x - rhs) <= tol * norm(rhs) * norm(A, 1)
-    
-    finalize(mumps)
-    MPI.Barrier(comm)
+  n = 20
+  A = sprand(Float64, n, n, 0.3) + I
+  A = A + A'
+  rhs = rand(Float64, n)
+
+  icntl = default_icntl[:]
+  icntl[1:4] .= 0
+  icntl[13] = 0
+
+  mumps = Mumps{Float64}(mumps_symmetric, icntl, default_cntl64)
+
+  @test mumps.icntl[13] == 0
+
+  factorize!(mumps, A)
+  x = solve(mumps, rhs)
+
+  tol = sqrt(eps(Float64))
+  @test norm(A * x - rhs) <= tol * norm(rhs) * norm(A, 1)
+
+  finalize(mumps)
+  MPI.Barrier(comm)
 end
 
 @testset "ScaLAPACK disabled (ICNTL[13]>0)" begin
-    n = 20
-    A = sprand(Float64, n, n, 0.3) + I
-    A = A + A'
-    rhs = rand(Float64, n)
-    
-    icntl = default_icntl[:]
-    icntl[1:4] .= 0
-    icntl[13] = 999
-    
-    mumps = Mumps{Float64}(mumps_symmetric, icntl, default_cntl64)
-    
-    @test mumps.icntl[13] == 999
-    
-    factorize!(mumps, A)
-    x = solve(mumps, rhs)
-    
-    tol = sqrt(eps(Float64))
-    @test norm(A * x - rhs) <= tol * norm(rhs) * norm(A, 1)
-    
-    finalize(mumps)
-    MPI.Barrier(comm)
+  n = 20
+  A = sprand(Float64, n, n, 0.3) + I
+  A = A + A'
+  rhs = rand(Float64, n)
+
+  icntl = default_icntl[:]
+  icntl[1:4] .= 0
+  icntl[13] = 999
+
+  mumps = Mumps{Float64}(mumps_symmetric, icntl, default_cntl64)
+
+  @test mumps.icntl[13] == 999
+
+  factorize!(mumps, A)
+  x = solve(mumps, rhs)
+
+  tol = sqrt(eps(Float64))
+  @test norm(A * x - rhs) <= tol * norm(rhs) * norm(A, 1)
+
+  finalize(mumps)
+  MPI.Barrier(comm)
 end
 
 @testset "ScaLAPACK with unsymmetric matrix" begin
-    n = 20
-    A = sprand(Float64, n, n, 0.3) + I
-    rhs = rand(Float64, n)
-    
-    icntl = default_icntl[:]
-    icntl[1:4] .= 0 
-    icntl[13] = 0
-    
-    mumps = Mumps{Float64}(mumps_unsymmetric, icntl, default_cntl64)
-    
-    @test mumps.icntl[13] == 0
-    
-    factorize!(mumps, A)
-    x = solve(mumps, rhs)
-    
-    tol = sqrt(eps(Float64))
-    @test norm(A * x - rhs) <= tol * norm(rhs) * norm(A, 1)
-    
-    finalize(mumps)
-    MPI.Barrier(comm)
+  n = 20
+  A = sprand(Float64, n, n, 0.3) + I
+  rhs = rand(Float64, n)
+
+  icntl = default_icntl[:]
+  icntl[1:4] .= 0
+  icntl[13] = 0
+
+  mumps = Mumps{Float64}(mumps_unsymmetric, icntl, default_cntl64)
+
+  @test mumps.icntl[13] == 0
+
+  factorize!(mumps, A)
+  x = solve(mumps, rhs)
+
+  tol = sqrt(eps(Float64))
+  @test norm(A * x - rhs) <= tol * norm(rhs) * norm(A, 1)
+
+  finalize(mumps)
+  MPI.Barrier(comm)
 end
 
 @testset "Modify ICNTL[13] with set_icntl!" begin
-    n = 20
-    A = sprand(Float64, n, n, 0.3) + I
-    A = A + A'
-    rhs = rand(Float64, n)
-    
-    icntl = default_icntl[:]
-    icntl[1:4] .= 0
+  n = 20
+  A = sprand(Float64, n, n, 0.3) + I
+  A = A + A'
+  rhs = rand(Float64, n)
 
-    mumps = Mumps{Float64}(mumps_symmetric, icntl, default_cntl64)
-    
-    MUMPS.set_icntl!(mumps, 13, 10; displaylevel = 0)
-    @test mumps.icntl[13] == 10
-    
-    factorize!(mumps, A)
-    x = solve(mumps, rhs)
-    
-    tol = sqrt(eps(Float64))
-    @test norm(A * x - rhs) <= tol * norm(rhs) * norm(A, 1)
-    
-    finalize(mumps)
-    MPI.Barrier(comm)
+  icntl = default_icntl[:]
+  icntl[1:4] .= 0
+
+  mumps = Mumps{Float64}(mumps_symmetric, icntl, default_cntl64)
+
+  MUMPS.set_icntl!(mumps, 13, 10; displaylevel = 0)
+  @test mumps.icntl[13] == 10
+
+  factorize!(mumps, A)
+  x = solve(mumps, rhs)
+
+  tol = sqrt(eps(Float64))
+  @test norm(A * x - rhs) <= tol * norm(rhs) * norm(A, 1)
+
+  finalize(mumps)
+  MPI.Barrier(comm)
 end
