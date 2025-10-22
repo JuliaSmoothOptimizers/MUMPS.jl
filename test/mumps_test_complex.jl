2e74567502ae78b4c35800079632675533c3c507
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-10-23 00:58:53 +0530

    Update mumps_test_complex.jl

--- test/mumps_test_complex.jl
+++ test/mumps_test_complex.jl
@@ -7,7 +7,7 @@ tol = sqrt(eps(Float64))
 
 mumps1 = quiet_mumps(ComplexF64; sym = mumps_definite)
 A = sparse(Diagonal(([1.0, 2.0, 3.0, 4.0])))
-factorize!(mumps1, A)
+factorize!(mumps1, A)  # Analyze and factorize.
 rhs = [1.0, 4.0, 9.0, 16.0]
 x = solve(mumps1, rhs)
 finalize(mumps1)
@@ -17,7 +17,7 @@ MPI.Barrier(comm)
 mumps1_unsafe = quiet_mumps(ComplexF64; sym = mumps_definite)
 A = sparse(Diagonal(Array{ComplexF64}([1.0, 2.0, 3.0, 4.0])))
 associate_matrix!(mumps1_unsafe, A; unsafe = true)
-factorize!(mumps1_unsafe);
+factorize!(mumps1_unsafe);  # Analyze and factorize.
 rhs = Array{ComplexF64}([1.0, 4.0, 9.0, 16.0])
 orig_rhs = copy(rhs)
 associate_rhs!(mumps1_unsafe, rhs; unsafe = true)
@@ -49,7 +49,7 @@ MPI.Barrier(comm)
 mumps3_unsafe = Mumps{ComplexF64}(mumps_unsymmetric, icntl, default_cntl32);
 A = sparse(ComplexF64[1.0+0.1im 0.5 0.2 0.0; 0.3 2.0+0.2im 0.5 0.1; 0.0 0.4 3.0+0.3im 0.5; 0.1 0.0 0.3 4.0+0.4im])
 associate_matrix!(mumps3_unsafe, A; unsafe = true)
-factorize!(mumps3_unsafe);
+factorize!(mumps3_unsafe);  # Analyze and factorize.
 rhs = Array{ComplexF64}([1.0, 4.0, 9.0, 16.0])
 orig_rhs = copy(rhs)
 associate_rhs!(mumps3_unsafe, rhs; unsafe = true)
@@ -59,6 +59,8 @@ get_sol!(x, mumps3_unsafe)
 finalize(mumps3_unsafe)
 MPI.Barrier(comm)
 @test(norm(A * x - orig_rhs) <= tol * norm(orig_rhs) * norm(A, 1))
+
+# Test convenience interface.
 
 n = 10
 n3 = n * n * n
