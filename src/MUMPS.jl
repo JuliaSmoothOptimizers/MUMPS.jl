cfeabb301cda31dbdbcda0e0f92e6093bbb084bc
amontoison <amontoison@users.noreply.github.com>
2026-01-15 17:52:09 +0000

    :robot: Format .jl files

--- src/MUMPS.jl
+++ src/MUMPS.jl
@@ -45,11 +45,11 @@ else
 end
 
 function __init__()
-    config = LinearAlgebra.BLAS.lbt_get_config()
-    if !any(lib -> lib.interface == :lp64, config.loaded_libs)
-        LinearAlgebra.BLAS.lbt_forward(OpenBLAS32_jll.libopenblas_path)
-    end
-    return
+  config = LinearAlgebra.BLAS.lbt_get_config()
+  if !any(lib -> lib.interface == :lp64, config.loaded_libs)
+    LinearAlgebra.BLAS.lbt_forward(OpenBLAS32_jll.libopenblas_path)
+  end
+  return
 end
 
 include("mumps_types.jl")
